84250ffbf77c81b6e505b02d9c6fd5f659a85c66
Ladan Nazari <ladannazary@gmail.com>
2022-10-25 16:15:28 +0330

    Signup Form Component and Redux State Done

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -32,6 +32,30 @@ export const login = (user) => async (dispatch) => {
 
 const initialState = { user: null };
 
+// restore session user thunk action
+export const restoreUser = () => async dispatch => {
+    const response = await csrfFetch('/api/session');
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+  };
+
+// sign up thunk action
+export const signup = (user) => async (dispatch) => {
+    const { username, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+  };
+
 const sessionReducer = (state = initialState, action) => {
   let newState;
   switch (action.type) {
