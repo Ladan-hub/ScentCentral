f77e0e2df09b40a79c3d03f771828b787218fba7
Ladan Nazari <ladannazary@gmail.com>
2022-11-02 03:18:28 +0330

    CSS Done

--- frontend/src/components/LoginFormModal/LoginForm.js
+++ frontend/src/components/LoginFormModal/LoginForm.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import * as sessionActions from "../../store/session";
 import { useDispatch } from "react-redux";
+import "./LoginForm.css"
+
 
 function LoginForm() {
   const dispatch = useDispatch();
@@ -20,32 +22,47 @@ function LoginForm() {
   };
 
   return (
+    <main className="form-root">
+      <div className="form-container-login">
     <form onSubmit={handleSubmit}>
+    <main className="fields-container">
+    <div className="error-message-container">
       <ul>
         {errors.map((error, idx) => (
           <li key={idx}>{error}</li>
         ))}
       </ul>
+      </div>
+      <div className="label">
       <label>
-        Username or Email
-        <input
+        <input className="input"
+        placeholder="Username or Email"
           type="text"
           value={credential}
           onChange={(e) => setCredential(e.target.value)}
           required
         />
       </label>
+      </div>
+      <div className="label">
       <label>
-        Password
-        <input
+        
+        <input className="input"
+        placeholder="Password"
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           required
         />
       </label>
-      <button type="submit">Log In</button>
+      </div>
+      <div className="signup-button-container" >
+      <button className="login-button" type="submit">Log In</button>
+      </div>
+      </main>
     </form>
+    </div>
+    </main>
   );
 }
 
