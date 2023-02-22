0c3dd0540be5e368705d09c369144dd4dfafb517
Ladan Nazari <ladannazary@gmail.com>
2023-02-22 20:01:08 +0330

    review form css update

--- frontend/src/components/CreateReviewForm/index.js
+++ frontend/src/components/CreateReviewForm/index.js
@@ -20,34 +20,34 @@ const ReviewForm = () => {
 
   // useStates
   const [content, setContent] = useState("");
-  const [stars, setStars] = useState(0);
   const [validationErrors, setValidationErrors] = useState([]);
 
   // Form Validations
   useEffect(() => {
     const errors = [];
-    if (content.length < 3) {
+    if (content === "") {
+      errors.push("")
+    }
+    else if (content.length < 3) {
       errors.push("Review must be at least 3 chatracters");
     }
     if (content.length > 255) {
-      errors.push("Rview must be less than 256 characters")
+      errors.push("Rview must be less than 256 characters");
     }
     setValidationErrors(errors);
   }, [content]);
 
   // Submit review button event handler
   const reviewSubmitted = (e) => {
-    
     e.preventDefault();
 
     const review = {
       content,
-      stars,
       userId: loggedInUser.id,
       boutiqueId: boutiqueId,
     };
 
-    console.log("THIS IS THE NEW REVIEW", review)
+    console.log("THIS IS THE NEW REVIEW", review);
 
     dispatch(createReviewThunk(review));
     reset();
@@ -58,58 +58,52 @@ const ReviewForm = () => {
     history.push(`/boutiques/${boutiqueId}`);
   };
 
-  const starCount = (num) => {
-
-    setStars(num)
-
-}
+  
 
   return (
     loggedInUser && (
-      <main className="form-root">
-      <div className="review-form-container">
-        <div className="error-message-container">
-        <ul className="error-messages-ul">
-          {validationErrors.map((validationError) => (
-            <li className="add-review-form-errors-li" key={validationError}>{validationError}</li>
-          ))}
-        </ul>
+      <>
+        <div className="form-title-banner">
+          <h1 className="form-banner-text">Leave Review</h1>
         </div>
-        <form className="review-form" onSubmit={reviewSubmitted}>
-          <main className="fields-container">
-            <div className="add-review-textarea">
-          <textarea
-            className="review-field"
-            value={content}
-            name="review"
-            placeholder="Review"
-            onChange={(e) => setContent(e.target.value)}
-          ></textarea>
+        <main className="form-root">
+          <div className="review-form-container">
+            <div className="error-message-container">
+              <ul className="error-messages-ul">
+                {validationErrors.map((validationError) => (
+                  <li
+                    className="add-review-form-errors-li"
+                    key={validationError}
+                  >
+                    {validationError}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <form className="add-boutique-form" onSubmit={reviewSubmitted}>
+              <label className="form-label">
+                <textarea
+                  className="review-field"
+                  value={content}
+                  name="review"
+                  placeholder="Review"
+                  onChange={(e) => setContent(e.target.value)}
+                ></textarea>
+              </label>
+
+              <div className="review-button-container">
+                <button
+                  disabled={validationErrors.length > 0}
+                  className="add-new-review-button"
+                  type="submit"
+                >
+                  Create Review
+                </button>
+              </div>
+            </form>
           </div>
-          <div className="stars-and-button-container">
-          {/* <div className='star-container'>
-                                            {stars < 1 && <i id='noStar1' onClick={() => starCount(1)} className="fa-regular fa-star"></i>}
-                                            {stars > 0 && <i id='star1' onClick={() => starCount(1)} className="fa-solid fa-star"></i>}
-                                            {stars < 2 && <i id='noStar2' onClick={() => starCount(2)} className="fa-regular fa-star"></i>}
-                                            {stars > 1 && <i id='star2' onClick={() => starCount(1)} className="fa-solid fa-star"></i>}
-                                            {stars < 3 && <i id='noStar3'  onClick={() => starCount(3)} className="fa-regular fa-star"></i>}
-                                            {stars > 2 && <i id='star3'  onClick={() => starCount(2)} className="fa-solid fa-star"></i>}
-                                            {stars < 4 && <i id='noStar4'  onClick={() => starCount(4)} className="fa-regular fa-star"></i>}
-                                            {stars > 3 && <i id='star4' onClick={() => starCount(3)} className="fa-solid fa-star"></i>}
-                                            {stars < 5 && <i id='noStar5' onClick={() => starCount(5)} className="fa-regular fa-star"></i>}
-                                            {stars > 4 && <i id='star5'  onClick={() => starCount(4)} className="fa-solid fa-star"></i>}
-                                        </div> */}
-        
-          {/* <div className="add-review-button-container"> */}
-          <button disabled={validationErrors.length > 0} className="add-new-review-button" type="submit">
-            Create Review
-          </button>
-          {/* </div> */}
-          </div>
-          </main>
-        </form>
-      </div>
-      </main>
+        </main>
+      </>
     )
   );
 };
